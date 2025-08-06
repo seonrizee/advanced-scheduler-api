063755f4bbf63dafdca5ec8b47c6760ca63b7ea3
seonrizee <seonrizee@gmail.com>
2025-08-07 02:49:49 +0900

    feat(lv0): Spring Boot 프로젝트 초기화 및 MySQL 연동 설정
    - Spring Boot 프로젝트 기본 구조 생성
    - `application.yml`을 이용한 MySQL 데이터베이스 연동 설정
    - `.env` 파일을 사용하여 데이터베이스 접속 정보 관리
    - 보안을 위해 `.env`를 `.gitignore`에 추가

---
+++ src/main/java/io/github/seonrizee/scheduler/AdvancedSchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.seonrizee.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSchedulerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedSchedulerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/seonrizee/scheduler/AdvancedSchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.seonrizee.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedSchedulerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
