ffa01f3b391fc3252a22068648bf8b52d0675857
seonrizee <seonrizee@gmail.com>
2025-08-11 11:12:14 +0900

    refactor(lv4): user repository default 메서드 사용
     -default 메서드 사용과 별도의 조회 전용 클래스 생성에 대해 추가 검토 필요

--- src/main/java/io/github/seonrizee/scheduler/repository/UserRepository.java
+++ src/main/java/io/github/seonrizee/scheduler/repository/UserRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // TODO: default 메서드의 올바른 사용법인지 검토 필요
     default User findByIdOrThrow(Long userId) {
         return findById(userId)
                 .orElseThrow(() -> new CustomBusinessException(ErrorCode.USER_NOT_FOUND));
