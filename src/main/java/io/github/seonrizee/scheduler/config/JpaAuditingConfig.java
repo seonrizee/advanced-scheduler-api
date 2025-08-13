d5ba4377c48d81109c1b3477dae30e228c0889b2
seonrizee <seonrizee@gmail.com>
2025-08-13 14:32:09 +0900

    refactor: JpaAuditingConfig 별도 생성

---
+++ src/main/java/io/github/seonrizee/scheduler/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package io.github.seonrizee.scheduler.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
